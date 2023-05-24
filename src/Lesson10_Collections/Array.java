94e2ad133ce985462681b78df9c0d9cd23be56cc
Angelina <a.hurok@gmail.com>
2023-05-24 18:48:23 +0300

    added class Array with necessary functions. TODO - add exeptions

---
+++ src/Lesson10_Collections/Array.java
@@ -0,0 +1,33 @@
+package Lesson10_Collections;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Array {
+    private ArrayList<Double> arr;
+    public Array(){ this.arr = new ArrayList<>();}
+    //input values
+    public void inputValues(){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input amount of elements you want to add: ");
+        int amount = in.nextInt();
+        double value;
+
+        for(int i = 0; i < amount; i++){
+            value = in.nextDouble();
+            this.arr.add(value);
+        }
+    }
+    public void centeringArray(){
+        for(int i = 0; i < this.arr.size(); i++){
+            this.arr.set(i, this.arr.get(i)/2);
+        }
+    }
+
+    public void printArr(){
+        for(int i = 0; i < this.arr.size(); i++){
+            System.out.println(this.arr.get(i));
+        }
+        System.out.println("\n");
+    }
+}
---
+++ src/Lesson10_Collections/Lesson10.java
@@ -0,0 +1,11 @@
+package Lesson10_Collections;
+
+public class Lesson10 {
+    public static void main(String[] args) {
+        Array arr = new Array();
+        arr.inputValues();
+        arr.printArr();
+        arr.centeringArray();
+        arr.printArr();
+    }
+}
